c4f99f5f94bce816fe5122764d6864abb9ca7126
Walter Gutierrez <elterrible8596@hotmail.com>
2022-03-30 10:46:49 -0400

    Database creation

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,21 @@
-const express = require ("express");
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import fileupload from 'express-fileupload';
+import history from 'connect-history-api-fallback';
+import path from 'path';
+
+//const express = require ("express");
 const app = express ();
 
 //Middlewares
-
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(fileupload({userTempFiles:true}));
+app.use(history());
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Routes
 app.get('/', (req, res)=>{
